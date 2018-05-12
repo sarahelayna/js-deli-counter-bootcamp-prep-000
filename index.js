54d7713873f44443a724878359446adb2ed472d7
Sarah F <sarahelayna@gmail.com>
2018-05-12 23:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ function takeANumber (katzDeli, name) {
     return "Welcome, " + name + ". You are number " + katzDeli.length + " in line.";
 }
 
-function nowServing () {
+function nowServing (katzDeli) {
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-      return katzDeli[0];
+      return "Currently serving " + katzDeli.shift() + ".";
   }
-  katzDeli.splice(0);
 }
 
 function currentLine (katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
-  } else {
-    return "The line is currently:"  katzDeli
+  } 
+  for (var i = 0; i < katzDeli.length; i++) {
+    names.push(`${i + 1}. ${katzDeli[i]}`)
   }
+  
+    return "The line is currently: ${names.join(', ')}"
 }
